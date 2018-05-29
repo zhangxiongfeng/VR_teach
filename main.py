2ea717f8f02e06b2412bf721633a768d34b39b4d
zhangxiongfeng <654934426@qq.com>
2018-05-29 16:32:03 +0800

    把重要数据私有化，以及增加了对服务器返回failed的报错，修改跳转信号的参数格式

--- main.py
+++ main.py
@@ -29,27 +29,42 @@ import cv2,time
 SERVER_URL = "ws://dev-accwssail.healthmall.cn/server/bodyAnaylzer/data"
 MID = "0001"
 
-# 声明用户信息为全局变量
-memberId = ""
-memberName = ""
-memHeadImg = ""
-sectionList = ""
-courseName = ""
 
-# 连续播放的次数
-play_times = 0
+class globalData(object):
+    def __init__(self):
+        # 初始化变量
+        self.global_dict = {
+            "memberId": "",
+            "memberName": "",
+            "memHeadImg": "",
+            "sectionList": "",
+            "courseName": "",
+            "play_times": 0
+        }
+
+    def set_data(self, data, value):
+        if data in self.global_dict.keys():
+            self.global_dict[data] = value
+        else:
+            print("您输入的变量[%s]不存在" % data)
+
+    def get_data(self, data):
+        if data in self.global_dict.keys():
+            return self.global_dict[data]
+
+        else:
+            print("您想获取的变量[%s]不存在" % data)
+
 # 初始界面
 class Scan_dialog(QtWidgets.QDialog, scan_dialog_ui.Ui_Dialog):
     def __init__(self, parent=None):
         super(Scan_dialog, self).__init__()
-        global memHeadImg
-        global memberName
-        global memberId
+        # global memHeadImg
+        # global memberName
+        # global memberId
 
         # TODO 初始化个人信息
-        memberName = ""
-        memberId = ""
-        memHeadImg = ""
+        data.__init__()
 
         # 生成二维码
         qrcodeInfo = "https://accwsiot.healthmall.cn/unAppOpen?id=AR" + MID
@@ -97,22 +112,19 @@ class Result_ui(QtWidgets.QDialog,result_ui.Ui_Dialog):
         self.timer_result.start(5 * 1000)
 
     def auto_finish(self):
-        j.jump(["jumpToScan", ])
+        j.jump("jumpToScan")
 
 # 创建跳转对象
 class Jump_to(object):
     def __init__(self):
-        pass
+        self.duration = 0
+        self.memberId = 0
+        self.memHeadImg = ""
+        self.memberName = ""
+        self.courseName = ""
+        self.sectionList = ""
 
-    def jump(self, messList):
-        global memHeadImg
-        global memberName
-        global memberId
-        global sectionList
-        global courseName
-
-        command = messList[0]
-
+    def jump(self, command):
         if command == "jumpToScan":
             # 关闭上层窗口,防止异常添加保护
             try:
@@ -161,9 +173,14 @@ class Jump_to(object):
         self.w2.reject()
         self.w3 = Player_ui()
 
+        # 获取全局变量
+        self.courseName = data.get_data("courseName")
+        self.memberName = data.get_data("memberName")
+        self.memHeadImg = data.get_data("memHeadImg")
+
         # 保存学生头像，覆盖默认图片
         path = os.getcwd() + '/AR教学视频v1.0/res/user_icon.jpg'
-        urllib.request.urlretrieve(memHeadImg, path)
+        urllib.request.urlretrieve(self.memHeadImg, path)
 
         # 设置学生头像
         self.w3.user_icon.setStyleSheet("border-image: url(:/img/AR教学视频v1.0/res/user_icon.jpg);\n"
@@ -171,13 +188,13 @@ class Jump_to(object):
 
         # TODO 需要老师名称和头像地址
         self.w3.tech_name.setText("<html><head/><body><p><span style=\" font-size:26pt; color:#ffffff;\">陈老师</span></p></body></html>")
-        self.w3.course_name.setText("<html><head/><body><p><span style=\" font-size:14pt; color:#fff;\">"+courseName+"</span></p></body></html>")
+        self.w3.course_name.setText("<html><head/><body><p><span style=\" font-size:14pt; color:#fff;\">"+self.courseName+"</span></p></body></html>")
         self.w3.cal_num.setText("<html><head/><body><p><span style=\" font-size:42pt; color:#fff;\">320</span><span style=\" font-size:22pt; color:#939a99;\">千卡</span></p></body></html>")
-        self.w3.user_name.setText( "<html><head/><body><p><span style=\" font-family:\'PingFang SC\'; font-size:26pt; color:#ffffff;\">"+memberName+"</span></p></body></html>")
+        self.w3.user_name.setText( "<html><head/><body><p><span style=\" font-family:\'PingFang SC\'; font-size:26pt; color:#ffffff;\">"+self.memberName+"</span></p></body></html>")
         self.w3.goal_num.setText("<html><head/><body><p><span style=\" font-size:42pt; color:#fff;\">89</span><span style=\" font-size:22pt; color:#939a99;\">/100分</span></p></body></html>")
 
-        # 加上视频播放功能
-        fileNameList = [os.getcwd()+'\some2.mp4', os.getcwd()+'\some3.mp4',]
+        # TODO 加上视频播放功能
+        fileNameList = [os.getcwd()+'\some2.mp4', os.getcwd()+'\some3.mp4', ]
 
         self.player = VideoWindow()
 
@@ -218,10 +235,10 @@ class Jump_to(object):
 # 创建服务器连接
 class recv_socket(QThread):
     # 声明信号，发送信号时传入list
-    trigger = pyqtSignal(list)
+    trigger = pyqtSignal(str)
 
     def __init__(self):
-        super(recv_socket,self).__init__()
+        super(recv_socket, self).__init__()
         pass
 
     # QThread的第二线程
@@ -238,14 +255,6 @@ class recv_socket(QThread):
         self.ws.run_forever()
 
     def on_message(self, ws, message_server):
-        # TODO 改变跳转传入参数
-
-        # 声明全局变量
-        global memberId
-        global memHeadImg
-        global memberName
-        global courseName
-        global sectionList
 
         # json转化为dict
         dict_message = json.loads(message_server)
@@ -254,22 +263,28 @@ class recv_socket(QThread):
         print(message)
 
         if message == "register success":
-            pass
             # TODO 需要一些保护机制
-
-        elif message == "register failed": 
             pass
+
+        elif message == "register failed":
+            print("设备注册失败,尝试重新发送注册信息")
+            print(self.ws)
+            self.on_open(self, self.ws)
+
+        elif "failed" in message:
+            message_split = message.split()
+            print("[%s]出错:[%s]" % (message_split[0], message_splits[1]))
 
         # 解锁界面
         elif message == "unlock":
-            memberId = dict_message["memberId"]
-            memberName = dict_message["memberName"]
-            memHeadImg = dict_message["memHeadImg"]
 
-            # 页面跳转
-            # 数据形式为跳转命令+传入参数
-            messList = ["jumpToWait", memberId, memberName, memHeadImg]
-            self.emit_signal(messList)
+            # 设置全局变量
+            data.set_data("memberId", dict_message["memberId"])
+            data.set_data("memberName", dict_message["memberName"])
+            data.set_data("memHeadImg", dict_message["memHeadImg"])
+
+            # 页面信号
+            self.trigger.emit("jumpToWait")
 
             # 以json形式返回message
             send_message = {"message": "unlock ok"}
@@ -279,22 +294,20 @@ class recv_socket(QThread):
             print("返回二维码界面")
 
             # 发送跳转信号
-            messList = ["jumpToScan",]
+            self.trigger.emit("jumpToScan")
 
-
-            self.emit_signal(messList)
             send_message = {"message": "lock ok"}
+
             ws.send(json.dumps(send_message))
 
         elif message == "executeList":
             print("启动视频")
 
-            sectionList = dict_message["sectionList"]
-            courseName = dict_message["courseName"]
+            data.set_data("courseName", dict_message["courseName"])
+            data.set_data("sectionList", dict_message["sectionList"])
 
             # 发射参数为命名+课程List+课程名称
-            messList = ["jumpToPlay", sectionList, courseName]
-            self.emit_signal(messList)
+            self.trigger.emit("jumpToPlay")
 
             # 以json形式返回message
             send_message = {"message": "executeList ok"}
@@ -302,18 +315,14 @@ class recv_socket(QThread):
 
         elif message == "closure":
             print("结束视频")
-            messList = ["jumpToResult", ]
 
             # 发送跳转信号
-            self.emit_signal(messList)
-
+            self.trigger.emit("jumpToResult")
             # 以json形式返回message
             send_message = {"message": "closure ok"}
 
             ws.send(json.dumps(send_message))
 
-        #TODO 长连接断开后需要自动建立连接
-      
     def on_error(self, ws, error):
         print(error)
 
@@ -324,10 +333,6 @@ class recv_socket(QThread):
     def on_close(self, ws):
         print("close:")
         print(ws)
-
-    # 发射信号
-    def emit_signal(self, messList):
-        self.trigger.emit(messList)
 
 # 视频播放
 class VideoWindow(QMainWindow):
@@ -343,9 +348,6 @@ class VideoWindow(QMainWindow):
         self.cameramode = self.camera.CaptureMode(2)
         self.cameraimgcap = QCameraImageCapture(self.camera)
         self.videoWidget = QVideoWidget()
-
-        self.scene1 = QGraphicsScene()
-        self.view1 = QGraphicsView(self.scene1)
 
         self.mediaPlayer.setVideoOutput(self.videoWidget)
         self.cameraviewfinder.show()
@@ -394,6 +396,7 @@ class VideoWindow(QMainWindow):
         if type(fileName) != list:
             clip = VideoFileClip(fileName)
             return clip.duration
+
         for f in fileName:
             clip = VideoFileClip(f)
             times = clip.duration
@@ -404,18 +407,12 @@ class VideoWindow(QMainWindow):
     def pause(self):
        self.mediaPlayer.pause()
 
-
-    # def displayImage(self, img):
-    #     self.scene1.clear()
-    #
-    #     pixMap = QPixmap(img)
-    #     w, h = pixMap.width(), pixMap.height()
-    #     self.scene1.addPixmap(pixMap)
-    #     self.view1.fitInView(QRectF(0, 0, w, h), Qt.KeepAspectRatio)
-    #     self.scene1.update()
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+
+    #初始化变量
+    data = globalData()
+
     w = Scan_dialog()
     w.show()
 
